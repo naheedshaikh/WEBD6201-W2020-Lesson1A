38e3231642a8f8ddb33d09936e56bc9f0ff4b69b
Naheed Shaikh <naheed.shaikh@dcmail.ca>
2020-01-12 23:42:18 -0500

    added Bootstrap and Font-Awesome

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,6 +4,23 @@
     function Start()
     {
         console.log("App Started...");
+
+        let firstHeading = document.getElementById("firstHeading");
+
+        firstHeading.textContent = "Goodbye Cruel World!";
+
+        // document.getElementById("firstHeadig").textContent = "Goodbye Cruel World!";
+
+        firstHeading.style.fontWeight = "bold";
+
+        let main = document.getElementsByTagName("main")[0];
+
+        //Configuaring my new elements
+        let newH2 = document.createElement("h2");
+        newH2.setAttribute("class", "Display-2");
+        newH2.textContent="secondary heading";
+
+        main.appendChild(newH2);
     }
 
     window.addEventListener("load",Start);
